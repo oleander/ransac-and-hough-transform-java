035017088099ad7d5a35e174c5bdf328d2865d05
Linus Oleander <linus@oleander.nu>
2013-01-10 18:46:51 +0100

    Implement getCircle

--- GeographicalPR/src/RANSAC.java
+++ GeographicalPR/src/RANSAC.java
@@ -1,20 +1,18 @@
 import java.awt.Point;
 import java.util.Set;
+import java.util.*;
 
 public class RANSAC {
-    
-    private Set<Point> data;
+    private ArrayList<Point> data;
     private int[] model;
     private int sampleSize = 3;
     private int maxIter;
     private int threshold;
     
+    public static void main(String[] args) { }
     
-    public static void main(String[] args) {
-    
-    }
-    
-    /*input:
+    /*
+    input:
     data - a set of observations
     model - a model that can be fitted to data 
     n - the minimum number of data required to fit the model
@@ -57,36 +55,101 @@ return best_model, best_consensus_set, best_error
 */
   
     public RANSACResult execute(){
-        
-//        maybe_inliers := n randomly selected values from data
-//        maybe_model := model parameters fitted to maybe_inliers
-//        consensus_set := maybe_inliers
-        
-        for (int i = 0; i < maxIter; i++) {
-            
+        ArrayList<Point> maybeInliers = null;
+        Circle maybeCircle = null;
+
+        // maybe_inliers := n randomly selected values from data
+        // maybe_model := model parameters fitted to maybe_inliers
+        // consensus_set := maybe_inliers
+
+        // while iterations < k
+        for (int i = 0; i < this.maxIter; i++) {
+            // maybe_inliers := n randomly selected values from data
+            maybeInliers = this.getNPoints();
+
+            // maybe_model := model parameters fitted to maybe_inliers
+            maybeCircle = this.getCircle(maybeInliers);
         }
- 
+
         return null;
     }
-	  
+
+    private Circle getCircle(ArrayList<Point> workingPoints){
+        double a = workingPoints.get(0).getX();
+        double b = workingPoints.get(0).getY();
+
+        double c = workingPoints.get(1).getX();
+        double d = workingPoints.get(1).getY();
+
+        double e = workingPoints.get(2).getX();
+        double f = workingPoints.get(2).getY();
+
+        double k = (1/2) * (( a * a + b * b)*(e - c) + (c * c + d * d) * (a - e) + (e * e + f * f) * (c - a)) / (b * (e - c) + d * (a - e) + f * (c - a));
+        double h = (1/2) * ((a * a + b * b) * (f - d) + (c * c + d * d) * (b - f) + (e * e + f * f) * (d - b)) / (a * (f - d) + c * (b - f) + e * (d - b)); 
+        double r = Math.sqrt(Math.pow(a - h, 2) + Math.pow(b - k, 2));
+
+        return new Circle(k, h, r);
+    }
+    
+    private ArrayList<Point> getNPoints(){
+        ArrayList<Integer> collectedNumbers = null;
+        ArrayList<Point> result             = null;
+        Random random                       = new Random();
+
+        for (int n = 0; n < this.sampleSize; n++) {
+            while(true){
+                int possibleNumber = random.nextInt(this.data.size());
+                if(!collectedNumbers.contains(possibleNumber)){
+                    collectedNumbers.add(possibleNumber);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < collectedNumbers.size(); i++) {
+            result.add(data.get(collectedNumbers.get(i)));
+        }
+
+        return result;
+    }
   
-  
-  
-  class RANSACResult {
+  private class RANSACResult {
       private int[] bestModel;
-	  private Set<Point> bestConsenusSet;
-	  private int best_score;
-	  
-	  public int[] getBestModel() {
-		return bestModel;
-	  }
-	  public Set<Point> getBestConsenusSet() {
-		return bestConsenusSet;
-	  }
-	  public int getBest_score() {
-		return best_score;
-	  }
-	  
-	  
+      private Set<Point> bestConsenusSet;
+      private int best_score;
+      
+      public int[] getBestModel() {
+        return bestModel;
+      }
+      public Set<Point> getBestConsenusSet() {
+        return bestConsenusSet;
+      }
+      public int getBest_score() {
+        return best_score;
+      }
+  }
+
+  private class Circle {
+    private double x;
+    private double y;
+    private double radius;
+
+    public Circle(double x, double y, double radius){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
   }
 }
