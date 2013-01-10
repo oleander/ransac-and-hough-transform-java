461e3fc02b7d496c85c0c08ede4909e57ebee34c
Linus Oleander <linus@oleander.nu>
2013-01-10 19:25:13 +0100

    Final implementation of #execute

--- GeographicalPR/src/RANSAC.java
+++ GeographicalPR/src/RANSAC.java
@@ -8,57 +8,16 @@ public class RANSAC {
     private int sampleSize = 3;
     private int maxIter;
     private int threshold;
-    
+    private int sufficientSize = 3;
     public static void main(String[] args) { }
-    
-    /*
-    input:
-    data - a set of observations
-    model - a model that can be fitted to data 
-    n - the minimum number of data required to fit the model
-    k - the number of iterations performed by the algorithm
-    t - a threshold value for determining when a datum fits a model
-    d - the number of close data values required to assert that a model fits well to data
-output:
-    best_model - model parameters which best fit the data (or nil if no good model is found)
-    best_consensus_set - data points from which this model has been estimated
-    best_error - the error of this model relative to the data 
-
-iterations := 0
-best_model := nil
-best_consensus_set := nil
-best_error := infinity
-while iterations < k 
-    maybe_inliers := n randomly selected values from data
-    maybe_model := model parameters fitted to maybe_inliers
-    consensus_set := maybe_inliers
-
-    for every point in data not in maybe_inliers 
-        if point fits maybe_model with an error smaller than t
-            add point to consensus_set
-    
-    if the number of elements in consensus_set is > d 
-        (this implies that we may have found a good model,
-        now test how good it is)
-        this_model := model parameters fitted to all points in consensus_set
-        this_error := a measure of how well this_model fits these points
-        if this_error < best_error
-            (we have found a model which is better than any of the previous ones,
-            keep it until a better one is found)
-            best_model := this_model
-            best_consensus_set := consensus_set
-            best_error := this_error
-     
-    increment iterations
-
-return best_model, best_consensus_set, best_error
-*/
   
     public RANSACResult execute(){
-        ArrayList<Integer> maybeInliers = null;
-        ArrayList<Point> consensusSet   = null;
-        Circle maybeCircle              = null;
-        
+        ArrayList<Integer> maybeInliers   = null;
+        ArrayList<Point> consensusSet     = null;
+        ArrayList<Point> bestConsensusSet = null;
+        Circle bestCircle                 = null;
+        Circle maybeCircle                = null;
+
         // while iterations < k
         for (int i = 0; i < this.maxIter; i++) {
             // maybe_inliers := n randomly selected values from data
@@ -83,11 +42,20 @@ return best_model, best_consensus_set, best_error
                 if(offset < this.threshold){
                     // consensus_set := maybe_inliers
                     consensusSet.add(point);
+                    
+                    if(consensusSet.size() > this.sufficientSize) { break; }
                 }
             }
+
+            if(consensusSet.size() > bestConsensusSet.size()){
+                bestConsensusSet = consensusSet;
+                bestCircle = maybeCircle;
+            }
+
+            if(consensusSet.size() > this.sufficientSize) { break; }
         }
 
-        return null;
+        return new RANSACResult(bestConsensusSet, bestCircle);
     }
 
     private Circle getCircle(ArrayList<Integer> workingIndexes){
@@ -132,18 +100,20 @@ return best_model, best_consensus_set, best_error
     }
   
   private class RANSACResult {
-      private int[] bestModel;
-      private Set<Point> bestConsenusSet;
-      private int best_score;
-      
-      public int[] getBestModel() {
-        return bestModel;
+      private Circle circle;
+      private ArrayList<Point> consensusSet;
+
+      public RANSACResult(ArrayList<Point> consensusSet, Circle circle) {
+        this.consensusSet = consensusSet;
+        this.circle = circle;
       }
-      public Set<Point> getBestConsenusSet() {
-        return bestConsenusSet;
+
+      public Circle getCircle() {
+        return this.circle;
       }
-      public int getBest_score() {
-        return best_score;
+
+      public ArrayList<Point> getConsensusSet(){
+        return this.consensusSet;
       }
   }
 
