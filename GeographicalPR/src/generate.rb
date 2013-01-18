ca2e5a2014aab19365e68fe05667efb4773ee6c3
Linus Oleander <linus@oleander.nu>
2013-01-18 18:09:06 +0100

    Generate 4 circles

--- GeographicalPR/src/generate.rb
+++ GeographicalPR/src/generate.rb
@@ -1,4 +1,4 @@
-OFFSET_PERCENT = 30
+OFFSET_PERCENT = 0
 def mess(x)
    x + (x * rand(OFFSET_PERCENT) / 100.0) * [-1, 1].sample
 end
@@ -6,9 +6,28 @@ end
 def c(a,b,r); tt = t; [(a + r * Math.cos(tt)).round.to_i, (b + r * Math.sin(tt)).round.to_i]; end
 def t; rand(100) / 100.0 * 2 * Math::PI; end
 
-raw = 30.times.map do 
-  x, y = c(0, 0, 300)
+raw = 100.times.map do 
+  x, y = c(0, 0, 50)
   [mess(x).round.to_i, mess(y).round.to_i, "true"].join(",") 
+end
+
+raw += 100.times.map do 
+  x, y = c(100, 140, 110)
+  [mess(x).round.to_i, mess(y).round.to_i, "true"].join(",") 
+end
+
+raw += 100.times.map do 
+  x, y = c(0, 50, 50)
+  [mess(x).round.to_i, mess(y).round.to_i, "true"].join(",") 
+end
+
+raw += 100.times.map do 
+  x, y = c(20, 100, 100)
+  [mess(x).round.to_i, mess(y).round.to_i, "true"].join(",") 
+end
+
+raw += 100.times.map do 
+  [rand(200) * [-1, 1].sample, rand(200) * [-1, 1].sample, "true"].join(",") 
 end
 
 # raw += 150.times.map do
