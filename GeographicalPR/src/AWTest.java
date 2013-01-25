70b8d2329b9484fc74288e0b70196f753e753a70
cambr <daniel.tell4@gmail.com>
2013-01-25 13:15:05 +0100

    awtest

--- GeographicalPR/src/AWTest.java
+++ GeographicalPR/src/AWTest.java
@@ -2,8 +2,19 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-class AWTest extends TestCase {
-  public static void main(String[] args) {
-    AccumulatorWrapper a = new AccumulatorWrapper(-10, 10, -10, 10, 20, 50, 5, 5);
-  }
+public class AWTest extends TestCase {
+	
+	
+	
+	private AccumulatorWrapper aw;
+	
+	public void setUp(){
+		aw = new AccumulatorWrapper(-10, 10, -10, 10, 20, 50, 5, 5);
+	}
+	
+	public void testInitGet(){
+		
+		assertEquals(aw.get(0, 0, 0), 0);
+	}
+
 }
