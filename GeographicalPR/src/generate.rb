be18a8a90f29ddefecdafa437f10bcce5a80f4eb
Linus Oleander <linus@oleander.nu>
2013-01-25 23:12:07 +0100

    Data

--- GeographicalPR/src/generate.rb
+++ GeographicalPR/src/generate.rb
@@ -1,4 +1,4 @@
-OFFSET_PERCENT = 0
+OFFSET_PERCENT = 1
 def mess(x)
    x + (x * rand(OFFSET_PERCENT) / 100.0) * [-1, 1].sample
 end
@@ -6,30 +6,31 @@ end
 def c(a,b,r); tt = t; [(a + r * Math.cos(tt)).round.to_i, (b + r * Math.sin(tt)).round.to_i]; end
 def t; rand(100) / 100.0 * 2 * Math::PI; end
 
-m = 50
-# raw = m.times.map do 
-#   x, y = c(0, 0, 50)
-#   [mess(x).round.to_i, mess(y).round.to_i].join(",") 
-# end
+m = 100
 
-# raw += m.times.map do 
-#   x, y = c(100, 140, 110)
-#   [mess(x).round.to_i, mess(y).round.to_i].join(",") 
-# end
-
-# raw += m.times.map do 
-#   x, y = c(0, 50, 50)
-#   [mess(x).round.to_i, mess(y).round.to_i].join(",") 
-# end
-
-raw = (2 * m).times.map do 
-  x, y = c(20, 100, 100)
+raw = m.times.map do 
+  x, y = c(0, 0, 50)
   [mess(x).round.to_i, mess(y).round.to_i].join(",") 
 end
 
-# raw += 900.times.map do 
-#   [rand(200) * [-1, 1].sample, rand(200) * [-1, 1].sample, "true"].join(",") 
+raw += m.times.map do 
+  x, y = c(-80, -100, 50)
+  [mess(x).round.to_i, mess(y).round.to_i].join(",") 
+end
+
+raw += m.times.map do 
+  x, y = c(80, 120, 90)
+  [mess(x).round.to_i, mess(y).round.to_i].join(",") 
+end
+
+# raw += (2 * m).times.map do 
+#   x, y = c(1, -130, 100)
+#   [mess(x).round.to_i, mess(y).round.to_i].join(",") 
 # end
+
+raw += 50.times.map do 
+  [rand(200) * [-1, 1].sample, rand(200) * [-1, 1].sample].join(",") 
+end
 
 # raw += 150.times.map do
 #   [rand(800).round.to_i * [-1, 1].sample, rand(800).round.to_i * [-1, 1].sample , "false"].join(",") 
