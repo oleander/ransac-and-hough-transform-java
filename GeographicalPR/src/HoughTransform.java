f6ffd1b51248ce60d32d5aad112e6011b88d5404
cambr <daniel.tell4@gmail.com>
2013-01-25 20:25:45 +0100

    cord -> coord

--- GeographicalPR/src/HoughTransform.java
+++ GeographicalPR/src/HoughTransform.java
@@ -15,12 +15,12 @@ public class HoughTransform {
     private final int maxRadius         = 130;
     
     /* Max and minimal center x coordinates for the given circles */
-    private final int minXCord          = -200;
-    private final int maxXCord          = 200;
+    private final int minXCoord          = -200;
+    private final int maxXCoord          = 200;
       
     /* Max and minimal center y coordinates for the given circles */
-    private final int minYCord          = -200;
-    private final int maxYCord          = 200;
+    private final int minYCoord          = -200;
+    private final int maxYCoord          = 200;
   
     private final int radiusSize        = 6;
     private final int cellSize          = 6;
@@ -71,10 +71,10 @@ public class HoughTransform {
         }
 
         this.pixels = new AccumulatorWrapper(
-            this.minXCord, 
-            this.maxXCord, 
-            this.minYCord, 
-            this.maxYCord, 
+            this.minXCoord, 
+            this.maxXCoord, 
+            this.minYCoord, 
+            this.maxYCoord, 
             this.minRadius,
             this.maxRadius,
             this.cellSize,
@@ -194,20 +194,20 @@ public class HoughTransform {
         double y                  = point.getY();
 
         for (int r = this.minRadius; r < this.maxRadius; r++) {
-            for (int b = this.minYCord; b < this.maxYCord; b++) {
+            for (int b = this.minYCoord; b < this.maxYCoord; b++) {
                 double res = -1 * b * b + 2 * b * y + r * r - y * y;
                 if(res < 0) continue;
 
                 double a1 = x - Math.sqrt(res);
                 double a2 = x + Math.sqrt(res);
 
-                if(this.minXCord > a1) continue;
-                if(this.maxXCord < a1) continue;
+                if(this.minXCoord > a1) continue;
+                if(this.maxXCoord < a1) continue;
 
                 int a11 = (int) Math.round(a1);
                 int a22 = (int) Math.round(a2);
-                if(a11 < this.minXCord || a11 > this.maxXCord) continue;
-                if(a22 < this.minXCord || a22 > this.maxXCord) continue;
+                if(a11 < this.minXCoord || a11 > this.maxXCoord) continue;
+                if(a22 < this.minXCoord || a22 > this.maxXCoord) continue;
 
                 circles.add(new Circle(a11, b, r));
                 circles.add(new Circle(a22, b, r));
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Java implementation of the Hough transform and RANSAC algorithm
 
 ## RANSAC
 
+We start by choosing 3 random points from the data set and construct a circle that passes through them. Then we add all other points that are within a given distance from the circle's circumference. We repeat this procedure maxIter times, while keeping track of the highest number of points for a circle so far. When the iterations have finished, the circle with highest number of points is chosen as the best model.
+
 ### Methods
 
 - (+) showCanvas() *Displays the data points and the calculated circles on a canvas.*
@@ -30,6 +32,8 @@ Points from *points.r.data* are being used.
 ![PS3](http://i.imgur.com/RR8M50p.png)
 
 ## Hough Transform
+
+A 3D array - the accumulator - is used to store information about potential circles. The parameters are the x and y coordiantes of the center point and the radius For every point in the data set, the value of every cell that could represent the circle that passes through the point is incremented. Cells that exceed a certain threshold are chosen as candidate circles. ??????????????????????????????????????????
 
 ### Methods
 
